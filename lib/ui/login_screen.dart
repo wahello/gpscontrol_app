55e609cfd3d9c474146cdadbdcb2dddec4fc6904
Gab <gabosoft>
2019-12-13 14:55:47 -0500

    remove listen methods in splash and set into login screen old

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:GPS_CONTROL/models/users.dart';
+import 'package:GPS_CONTROL/pages/home.dart';
 import 'package:GPS_CONTROL/ui/SplashScreen.dart';
 import 'package:GPS_CONTROL/ui/custom_route.dart';
 import 'package:GPS_CONTROL/ui/dashboard_screen.dart';
@@ -131,169 +132,16 @@ class _LoginWebview extends State<LoginWebview> {
             appBar: new AppBar(
               title: Text('Iniciar Sesion'),
             ),
-            body: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                FlutterLogin(
-                    title: Constants.appName,
-                    logo: 'assets/icon/favicon0.png',
-                    logoTag: Constants.logoTag,
-                    titleTag: Constants.titleTag,
-                    messages: LoginMessages(
-                      usernameHint: 'Usuario',
-                      passwordHint: 'Contraseña',
-                      confirmPasswordHint: 'Confirmar',
-                      loginButton: 'Iniciar Sesion',
-                      signupButton: 'Registrarme',
-                      forgotPasswordButton: 'Olvidaste la contraseña?',
-                      recoverPasswordButton: 'Ayuda',
-                      goBackButton: 'Volver',
-                      confirmPasswordError: 'No coincide!',
-                      recoverPasswordDescription: 'para recuperar el pass ... ',
-                      recoverPasswordSuccess: 'Contraseña rescatada con éxito!!',
-                    ),
-                    theme:  LoginTheme(
-                          primaryColor: Colors.blue,
-                          accentColor: Colors.blueAccent,
-                          errorColor: Colors.deepOrange,
-                          titleStyle: TextStyle(
-                            fontSize: 12.0,
-                            color: Colors.white,
-                            fontFamily: 'Quicksand',
-                          ),
-
-
-                          beforeHeroFontSize: 31,
-                          afterHeroFontSize: 20,
-                          bodyStyle: TextStyle(
-                            fontStyle: FontStyle.italic,
-                            decoration: TextDecoration.underline,
-
-                          ),
-                    //   textFieldStyle: TextStyle(
-                    //     color: Colors.orange,
-                    //     shadows: [Shadow(color: Colors.yellow, blurRadius: 2)],
-                    //   ),
-                    //   buttonStyle: TextStyle(
-                    //     fontWeight: FontWeight.w800,
-                    //     color: Colors.yellow,
-                    //   ),
-
-                          cardTheme: CardTheme(
-                            color: Colors.white,
-                            elevation: 30,
-                            margin: EdgeInsets.only(top: 2),
-                            shape: ContinuousRectangleBorder(
-                                borderRadius: BorderRadius.circular(100.0)),
-                          ),
-                    //   inputTheme: InputDecorationTheme(
-                    //     filled: true,
-                    //     fillColor: Colors.purple.withOpacity(.1),
-                    //     contentPadding: EdgeInsets.zero,
-                    //     errorStyle: TextStyle(
-                    //       backgroundColor: Colors.orange,
-                    //       color: Colors.white,
-                    //     ),
-                    //     labelStyle: TextStyle(fontSize: 12),
-                    //     enabledBorder: UnderlineInputBorder(
-                    //       borderSide: BorderSide(color: Colors.blue.shade700, width: 4),
-                    //       borderRadius: inputBorder,
-                    //     ),
-                    //     focusedBorder: UnderlineInputBorder(
-                    //       borderSide: BorderSide(color: Colors.blue.shade400, width: 5),
-                    //       borderRadius: inputBorder,
-                    //     ),
-                    //     errorBorder: UnderlineInputBorder(
-                    //       borderSide: BorderSide(color: Colors.red.shade700, width: 7),
-                    //       borderRadius: inputBorder,
-                    //     ),
-                    //     focusedErrorBorder: UnderlineInputBorder(
-                    //       borderSide: BorderSide(color: Colors.red.shade400, width: 8),
-                    //       borderRadius: inputBorder,
-                    //     ),
-                    //     disabledBorder: UnderlineInputBorder(
-                    //       borderSide: BorderSide(color: Colors.grey, width: 5),
-                    //       borderRadius: inputBorder,
-                    //     ),
-                    //   ),
-                    //   buttonTheme: LoginButtonTheme(
-                    //     splashColor: Colors.purple,
-                    //     backgroundColor: Colors.pinkAccent,
-                    //     highlightColor: Colors.lightGreen,
-                    //     elevation: 9.0,
-                    //     highlightElevation: 6.0,
-                    //     shape: BeveledRectangleBorder(
-                    //       borderRadius: BorderRadius.circular(10),
-                    //     ),
-                    //     // shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-                    //     // shape: CircleBorder(side: BorderSide(color: Colors.green)),
-                    //     // shape: ContinuousRectangleBorder(borderRadius: BorderRadius.circular(55.0)),
-                    //   ),
-                        ),
-                    emailValidator: (value) {
-                      //if (!value.contains('@') || !value.endsWith('.com')) {
-                      //  return "El correo electrónico debe contener '@' y terminar con '.com' ";
-                      //}
-                      return null;
-                    },
-                    passwordValidator: (value) {
-                      if (value.isEmpty) {
-                        return 'La contraseña esta vacía';
-                      }
-                      return null;
-                    },
-                    onLogin: (loginData) {
-                      print('Login info');
-                      print('Name: ${loginData.name}');
-                      print('Password: ${loginData.password}');
-                      var client = OdooClient("http://66.228.39.68:8069");
-                      client.authenticate(loginData.name, loginData.password, "smart_contro").then((auth) {
-                        if (auth.isSuccess) {
-                          print("Bienvenido ${auth.getUser().name}");
-                          print(auth.getSessionId());
-                          var name_user = auth.getUser().name;
-                          _save(auth.getSessionId(),name_user,loginData.name,loginData.password);
-                          isLoggedIn = true;
-                        } else {
-                          print("Algo salio mal. :s ");
-                          isLoggedIn = false;
-                        }
-                      });
-                      return _loginUser(isLoggedIn);
-                    },
-                    onSignup: (loginData) {
-                      print('Signup info');
-                      print('Name: ${loginData.name}');
-                      print('Password: ${loginData.password}');
-                      return _loginUser(isLoggedIn);
-                    },
-                    onSubmitAnimationCompleted: () {
-                      if (isLoggedIn == true) {
-                        Navigator.of(context).pushReplacement(FadePageRoute(
-                          builder: (context) => DashboardScreen(data: usuario,),
-                        ));
-                      }
-                    },
-                    onRecoverPassword: (name) {
-                      print('Recover password info');
-                      print('Name: $name');
-                      return _recoverPassword(name);
-                      // Show new password dialog
-                    },
-                    showDebugButtons: false,
-                  ),
-                WebviewScaffold(
+            body: WebviewScaffold(
                 url: widget.authUrl,
                 withJavascript: true,
                 hidden: true,
                 withZoom: false,
                 clearCache: true,
                 clearCookies: true,
-                initialChild: SplashScreen(),
+                initialChild: LoginScreen(),
 
-            )
-              ],
+
             ),
         );
     }
